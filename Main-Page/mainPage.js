a3d59c4d97e5c5d0c3d9a0f1fa98823c2ff5c05e
unknown <elias78210m@gmail.com>
2024-01-18 17:16:10 +0100

    ae

--- Main-Page/mainPage.js
+++ Main-Page/mainPage.js
@@ -1,66 +1,17 @@
-class Player {
-  constructor(num) {
-    this.OnTile = null;
-    switch (num) {
-      case 1:
-        this.image = "./image1.png";
-        break;
+var btnTab=document.getElementsByClassName('btn')
 
-      case 2:
-        this.image = "./image2.png";
-        break;
+Array.from(btnTab).forEach(element => {
+  var initialElement=element.style;
+  element.addEventListener('click', function() {
+    element.style.transform="scale(1,1.1)";
+    element.style.borderRadius="200px";
+    element.style.backgroundColor="yellow";
+    element.style.cursor = "url(screamingChicken.png), auto";
+    setTimeout(function() {
+      element.style=initialElement;
+    },350);
+  });
+});
 
-      default:
-        this.image = "./image3.png";
-        break;
-    }
-  }
-}
-class Tile {
-  constructor(x, y) {
-    this.X = x;
-    this.Y = y;
-    this.occupied = null;
-    this.generateElement();
-  }
-  occupiedBy(player) {
-    if (player.OnTile != null) player.OnTile.player = null;
-    this.occupied = player;
-    player.OnTile = this;
-  }
-
-  generateElement() {
-    this.element = document.createElement("button");
-    this.element.addEventListener("click", this.onClick.bind(this));
-    this.element.classList.add("tile")
-  }
-
-  onClick() {
-    alert("(" + this.X + "," + this.Y + ")");
-  }
-}
-class Border{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-    this.generateElement();
-  }
-  generateElement() {
-    this.element = document.createElement("button");
-    this.element.addEventListener("click", this.onClick.bind(this));
-    this.element.classList.add("border")
-  }
-  
-  onClick() {
-    alert("border : ");
-  }
-
-}
-
-Board = [];
-
-function getTile(x, y) {
-  return document.getElementById("game").childNodes[x].childNodes[y];
-}
 
 
